c749b72b230978de63ceef986fffd71f5f7d7d90
JK404 <mixjeka404@gmail.com>
2025-04-09 14:17:57 +0300

    added static files and navbar

--- src/core/settings.py
+++ src/core/settings.py
@@ -138,6 +138,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+# source for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# output for python manage.py collectstatic
+STATIC_ROOT = BASE_DIR / 'local-cdn'
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'prod-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
